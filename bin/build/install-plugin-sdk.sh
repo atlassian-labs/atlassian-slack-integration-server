00413f2f3f1c233599e9ccaa0cb38d912045a3cc
Mykhailo Vlasov <mvlasov@atlassian.com>
2020-07-08 20:00:42 +0300

    Added workflow for unit and integration test

--- bin/build/install-plugin-sdk.sh
+++ bin/build/install-plugin-sdk.sh
@@ -2,8 +2,8 @@
 set -ex
 trap 'set +ex' EXIT
 
-echo "deb https://packages.atlassian.com/debian/atlassian-sdk-deb/ stable contrib" >>/etc/apt/sources.list \
-  && wget https://packages.atlassian.com/api/gpg/key/public \
-  && apt-key add public \
-  && apt-get update \
-  && apt-get install atlassian-plugin-sdk
+sudo sh -c 'echo "deb https://packages.atlassian.com/debian/atlassian-sdk-deb/ stable contrib" >>/etc/apt/sources.list'
+wget https://packages.atlassian.com/api/gpg/key/public
+sudo apt-key add public
+sudo apt-get update
+sudo apt-get install atlassian-plugin-sdk
