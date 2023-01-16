c67e964d757a3bad724b18b9ab170703588dd0b2
Mykhailo Vlasov <mvlasov@atlassian.com>
2023-01-16 17:23:18 +0200

    Added Maven overriding to all affected workflows

--- bin/build/override-atlassian-maven.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# override Maven from Plugin SDK as it is too old to run Confluence 8 and Jira 9.5
-
-BUNDLED_MVN_HOME=$(mvn -v | grep "Maven home" | sed 's/Maven home: //')
-echo "ATLAS_MVN=$BUNDLED_MVN_HOME/bin/mvn" >> $GITHUB_ENV
