cdc9d0d7ddc7b3287bb7422b8594ce60d0577642
Yevhen Hrytsai <yhrytsai@atlassian.com>
2024-05-18 13:49:12 +0300

    Run unit tests skipping enforcer until moved out from milestone versions

--- bin/build/run-unit-tests.sh
+++ bin/build/run-unit-tests.sh
@@ -2,5 +2,7 @@
 set -ex
 trap 'set +ex' EXIT
 
+# TODO: Remove -Denforcer.skip=true after moving from milestone versions
+
 atlas-version
-atlas-mvn --batch-mode verify -P jacoco
+atlas-mvn --batch-mode verify -P jacoco -Denforcer.skip=true
