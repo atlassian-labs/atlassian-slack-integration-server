4a4f17d8f0f17f72cd659b94ba57b582224a7c07
Yevhen Hrytsai <yhrytsai@atlassian.com>
2025-05-07 02:58:00 +0300

    Fix libasound and libgtk

--- bin/build/install-int-test-libs.sh
+++ bin/build/install-int-test-libs.sh
@@ -3,4 +3,4 @@ set -ex
 trap 'set +ex' EXIT
 
 sudo apt-get update
-sudo apt-get -y install libdbus-glib-1-2 libxrender1 libxcomposite-dev libasound2 libgtk2.0-0 libgtk-3-0 xvfb
+sudo apt-get -y install libdbus-glib-1-2 libxrender1 libxcomposite-dev libasound2t64 libgtk2.0-0t64 libgtk-3-0t64 xvfb
