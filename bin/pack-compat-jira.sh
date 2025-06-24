30af6067c701407eeafcb9c13caf14947f39dbf0
Yevhen Hrytsai <yhrytsai@atlassian.com>
2025-06-25 02:29:27 +0300

    Remove jira-8-compat module

--- bin/pack-compat-jira.sh
+++ bin/pack-compat-jira.sh
@@ -2,7 +2,5 @@
 
 (
     cd "$( dirname "${BASH_SOURCE[0]}")/.." ;
-    atlas-mvn install --batch-mode -Dmaven.test.skip=true "$@" -pl com.atlassian.plugins:jira-8-compat,com.atlassian.plugins:jira-service-desk-compat,com.atlassian.plugins:jira-service-desk-compat-common,com.atlassian.plugins:jira-service-desk-4-compat,com.atlassian.plugins:jira-service-desk-compat-main && \
-    rm -rf jira-slack-server-integration/jira-slack-server-integration-plugin/target/classes/com/atlassian/plugin/slack/jira/compat && \
-    rm -f jira-slack-server-integration/jira-slack-server-integration-plugin/target/dependency-maven-plugin-markers/com.atlassian.plugins-jira-8-compat-jar-*
+    atlas-mvn install --batch-mode -Dmaven.test.skip=true "$@" -pl com.atlassian.plugins:jira-service-desk-compat,com.atlassian.plugins:jira-service-desk-compat-common,com.atlassian.plugins:jira-service-desk-4-compat,com.atlassian.plugins:jira-service-desk-compat-main
 )
