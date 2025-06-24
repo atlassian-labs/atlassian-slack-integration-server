be209c2be32dbb2ee1d4134859b883974181c97b
Yevhen Hrytsai <yhrytsai@atlassian.com>
2025-06-24 23:39:31 +0300

    Update atlassian-plugin-sdk installation

--- bin/build/override-plugin-sdk-maven.sh
+++ bin/build/override-plugin-sdk-maven.sh
@@ -4,4 +4,6 @@
 # Maven from Plugin SDK is incompatible with newest AMPS plugins used to run Confluence 8 and Jira 9.5
 
 BUNDLED_MVN_HOME=$(mvn -v | grep "Maven home" | sed 's/Maven home: //')
+mvn -v
+atlas-version
 echo "ATLAS_MVN=$BUNDLED_MVN_HOME/bin/mvn" >> $GITHUB_ENV
