7707f22b3be732ce9e6d0cc160faeb6350d3a0aa
Matthew Hanselman <mhanselman@atlassian.com>
2025-06-26 14:09:18 +1000

    Replace javax with jakarta (missed) and upgraded BBDC to 10.0.0-m8

--- slack-server-integration-common/src/test/java/com/atlassian/plugins/slack/admin/AbstractAdminServletTest.java
+++ slack-server-integration-common/src/test/java/com/atlassian/plugins/slack/admin/AbstractAdminServletTest.java
@@ -10,9 +10,9 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static org.mockito.Mockito.never;
