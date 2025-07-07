0fb3e8c860a77858a0e57d0f43a1e7393fed06be
Yevhen Hrytsai <yhrytsai@atlassian.com>
2025-07-07 12:37:59 +0300

    Clean up version commands

--- bin/build/override-plugin-sdk-maven.sh
+++ bin/build/override-plugin-sdk-maven.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# override Maven from Plugin SDK with the one bundled into Github Actions image;
-# Maven from Plugin SDK is incompatible with newest AMPS plugins used to run Confluence 8 and Jira 9.5
+# Override Maven from Plugin SDK with the one bundled into Github Actions image;
 
 BUNDLED_MVN_HOME=$(mvn -v | grep "Maven home" | sed 's/Maven home: //')
-mvn -v
-atlas-version
 echo "ATLAS_MVN=$BUNDLED_MVN_HOME/bin/mvn" >> $GITHUB_ENV
