5d1f7e95cdb4c71fda1f8040af3be32bd15cbb17
Yevhen Hrytsai <yhrytsai@atlassian.com>
2025-08-16 23:08:49 +0300

    Replace snap Firefox with ESR

--- bin/build/install-int-test-libs.sh
+++ bin/build/install-int-test-libs.sh
@@ -4,3 +4,14 @@ trap 'set +ex' EXIT
 
 sudo apt-get update
 sudo apt-get -y install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+
+sudo snap remove firefox || true
+sudo add-apt-repository -y ppa:mozillateam/ppa
+echo '
+Package: firefox*
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 501
+' | sudo tee -a /etc/apt/preferences.d/mozillateamppa
+
+sudo apt-get update
+sudo apt-get -y install firefox-esr
