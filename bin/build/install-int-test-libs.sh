ae8534dcf49f0aa228f0e1fcb59b188acaebbef6
Yevhen Hrytsai <yhrytsai@atlassian.com>
2025-08-16 20:59:45 +0300

    Update Firefox libraries installation and try Confluence ITs on ubuntu-latest

--- bin/build/install-int-test-libs.sh
+++ bin/build/install-int-test-libs.sh
@@ -3,4 +3,4 @@ set -ex
 trap 'set +ex' EXIT
 
 sudo apt-get update
-sudo apt-get -y install libdbus-glib-1-2 libxrender1 libxcomposite-dev libasound2 libgtk2.0-0 libgtk-3-0 xvfb
+sudo apt-get install libgtk2.0-0t64 libgtk-3-0t64 libgbm-dev libnotify-dev libnss3 libxss1 libasound2t64 libxtst6 xauth xvfb
