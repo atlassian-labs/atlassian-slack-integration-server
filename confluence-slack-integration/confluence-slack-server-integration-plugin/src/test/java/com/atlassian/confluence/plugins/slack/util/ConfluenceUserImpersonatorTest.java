5d63833003cccffc20df7dbd5ee641010e3ec18a
Jakub Trzebiatowski <jtrzebiatowski@atlassian.com>
2026-01-30 13:47:12 +0100

    DCPL-3817: Remove PowerMock - ConfluenceUserImpersonatorTest

--- confluence-slack-integration/confluence-slack-server-integration-plugin/src/test/java/com/atlassian/confluence/plugins/slack/util/ConfluenceUserImpersonatorTest.java
+++ confluence-slack-integration/confluence-slack-server-integration-plugin/src/test/java/com/atlassian/confluence/plugins/slack/util/ConfluenceUserImpersonatorTest.java
@@ -2,45 +2,44 @@ package com.atlassian.confluence.plugins.slack.util;
 
 import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
 import com.atlassian.confluence.user.ConfluenceUser;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.function.Supplier;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-@RunWith(PowerMockRunner.class)
-@PowerMockIgnore("javax.*")
-@PrepareForTest(AuthenticatedUserThreadLocal.class)
+@RunWith(MockitoJUnitRunner.class)
 public class ConfluenceUserImpersonatorTest {
     @Mock
     ConfluenceUser previousConfluenceUser;
     @Mock
     ConfluenceUser confluenceUser;
-    @Mock
-    Supplier action;
 
     @InjectMocks
     ConfluenceUserImpersonator target;
 
     @Test
     public void impersonateShouldSetActiveUser() {
-        PowerMockito.mockStatic(AuthenticatedUserThreadLocal.class);
-        when(AuthenticatedUserThreadLocal.get()).thenReturn(previousConfluenceUser);
+        AuthenticatedUserThreadLocal.set(previousConfluenceUser);
+
+        Supplier<Void> action = Mockito.mock();
+        doAnswer(invocation -> {
+            assertThat(AuthenticatedUserThreadLocal.get(), Matchers.equalTo(confluenceUser));
+            return null;
+        }).when(action).get();
 
         target.impersonate(confluenceUser, action, "some cause");
 
         verify(action).get();
-        PowerMockito.verifyStatic(AuthenticatedUserThreadLocal.class);
-        AuthenticatedUserThreadLocal.set(confluenceUser);
-        PowerMockito.verifyStatic(AuthenticatedUserThreadLocal.class);
-        AuthenticatedUserThreadLocal.set(previousConfluenceUser);
+
+        assertThat(AuthenticatedUserThreadLocal.get(), Matchers.equalTo(previousConfluenceUser));
     }
 }
